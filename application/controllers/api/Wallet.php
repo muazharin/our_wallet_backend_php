739b53944e01b2dc3d92d6c2c5f2dfc6f5d887da
Muazharin Alfan <alfanmuazharin@gmail.com>
2022-01-17 00:09:41 +0700

    update controller wallet dan create wallet model

---
+++ application/controllers/api/Wallet.php
@@ -0,0 +1,260 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Wallet extends REST_Controller
+{
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('M_wallet');
+	}
+	public function get_wallet_get($id = null){
+		try {
+			$result = $this->M_wallet->get_wallet($id);
+			if($result['status']){
+				$message = [
+					'status'=> true,
+					'message'=> $result['message'],
+					'data'=> $result['data'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_OK);
+			}else{
+				$message=[
+					'status'=>false,
+					'message'=> $result['message'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}
+		} catch (\Throwable $th) {
+			$message = [
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+		
+	}
+	public function create_wallet_post(){
+		try {
+			$this->form_validation->set_data($this->post());
+			$this->form_validation->set_rules('userId', 'User Id', 'required|trim|xss_clean');
+			$this->form_validation->set_rules('walletName', 'Wallet Name', 'required|trim|xss_clean');
+			$this->form_validation->set_rules('walletColor', 'Wallet Color', 'required|trim|xss_clean');
+			if (!$this->form_validation->run()) {
+				$message = [
+					'status' => false,
+					'message' => 'Lengkapi semua field dengan benar!',
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}else{
+				$result = $this->M_wallet->create_wallet();
+				if($result['status']){
+					$message = [
+						'status' => true,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_CREATED);
+				}else{
+					$message = [
+						'status' => false,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+				}
+				
+			}
+		} catch (\Throwable $th) {
+			$message=[
+				'status'=>false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function update_wallet_post($id = null){
+		try {
+			$this->form_validation->set_data($this->post());
+			$this->form_validation->set_rules('userId', 'User Id', 'required|trim|xss_clean');
+			$this->form_validation->set_rules('walletName', 'Wallet Name', 'required|trim|xss_clean');
+			$this->form_validation->set_rules('walletColor', 'Wallet Color', 'required|trim|xss_clean');
+			if (!$this->form_validation->run()) {
+				$message = [
+					'status' => false,
+					'message' => 'Lengkapi semua field dengan benar!',
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}else{
+				$result = $this->M_wallet->update_wallet($id);
+				if($result['status']){
+					$message = [
+						'status' => true,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_OK);
+				}else{
+					$message = [
+						'status' => false,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+				}
+				
+			}
+		} catch (\Throwable $th) {
+			$message=[
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function add_member_wallet_post($id = null){
+		try {
+			$this->form_validation->set_data($this->post());
+			$this->form_validation->set_rules('userId', 'User Id', 'required|trim|xss_clean');
+			$this->form_validation->set_rules('memberId', 'Member Id', 'required|trim|xss_clean');
+			if (!$this->form_validation->run()) {
+				$message = [
+					'status' => false,
+					'message' => 'Lengkapi semua field dengan benar!',
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}else{
+				$result = $this->M_wallet->add_member_wallet($id);
+				if($result['status']){
+					$message = [
+						'status' => true,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_OK);
+				}else{
+					$message = [
+						'status' => false,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+				}
+				
+			}
+		} catch (\Throwable $th) {
+			$message=[
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function get_user_to_wallet_get($id = null){
+		try {
+			$result = $this->M_wallet->get_user_to_wallet($id);
+			if($result['status']){
+				$message = [
+					'status'=> true,
+					'message'=> $result['message'],
+					'data'=> $result['data'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_OK);
+			}else{
+				$message=[
+					'status'=>false,
+					'message'=> $result['message'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}
+		} catch (\Throwable $th) {
+			$message = [
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function get_member_of_wallet_get($id = null){
+		try {
+			$result = $this->M_wallet->get_member_of_wallet($id);
+			if($result['status']){
+				$message = [
+					'status'=> true,
+					'message'=> $result['message'],
+					'data'=> $result['data'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_OK);
+			}else{
+				$message=[
+					'status'=>false,
+					'message'=> $result['message'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}
+		} catch (\Throwable $th) {
+			$message = [
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function confirm_member_wallet_post($id = null){
+		try {
+			$this->form_validation->set_data($this->post());
+			$this->form_validation->set_rules('confirm', 'Confirm', 'required|trim|xss_clean');
+			if (!$this->form_validation->run()) {
+				$message = [
+					'status' => false,
+					'message' => 'Lengkapi semua field dengan benar!',
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}else{
+				$result = $this->M_wallet->confirm_member_wallet($id);
+				if($result['status']){
+					$message = [
+						'status' => true,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_CREATED);
+				}else{
+					$message = [
+						'status' => false,
+						'message' => $result['message'],
+					];
+					$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+				}
+				
+			}
+		} catch (\Throwable $th) {
+			$message=[
+				'status'=>false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+	public function invited_check_wallet_get($id = null){
+		try {
+			$result = $this->M_wallet->invited_check_wallet($id);
+			if($result['status']){
+				$message = [
+					'status'=> true,
+					'message'=> $result['message'],
+					'data'=> $result['data'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_OK);
+			}else{
+				$message=[
+					'status'=>false,
+					'message'=> $result['message'],
+				];
+				$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+			}
+		} catch (\Throwable $th) {
+			$message = [
+				'status' => false,
+				'message' => $th->getMessage(),
+			];
+			$this->set_response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+}
---
+++ application/models/M_wallet.php
@@ -0,0 +1,230 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_Wallet extends CI_Model
+{
+	public function get_wallet($id){
+		$this->db->select('*');
+		$this->db->from('wallet');
+		$this->db->join('our_wallet','our_wallet.owWalletId = wallet.walletId', 'left');
+		$this->db->where('our_wallet.owUserId', $id);
+		$result = $this->db->get();
+		if($result){
+			$data = $result->result_array();
+			$res = [
+				'status' => true,
+				'message' => 'Berhasil menampilkan data',
+				'data' => $data,
+			];
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Gagal akses data',
+			];
+		}
+		return $res;
+	}
+	public function create_wallet(){
+		$userId = $this->input->post('userId');
+		$walletName = $this->input->post('walletName');
+		$walletColor = $this->input->post('walletColor');
+		$data = [
+			'walletName' => $walletName,
+			'walletMoney' => 0,
+			'walletColor' => $walletColor,
+			'walletCreatedAt' => date('Y-m-d H:i:s'),
+			'walletUpdatedAt' => date('Y-m-d H:i:s'),
+			'walletIsActive' => true,
+		];
+		if($this->db->insert('wallet', $data)){
+			$data1 = [
+				'owWalletId' => $this->db->insert_id(),
+				'owUserId' => $userId,
+				'owIsUserActive' => true,
+				'owIsAdmin' => true,
+				'owDate' => date('Y-m-d H:i:s'),
+			];
+			$this->db->insert('our_wallet', $data1);
+			$res = [
+				'status' => true,
+				'message' => 'Berhasil membuat wallet baru!',
+			];
+			return $res;
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Mohon maaf, terjadi kesalahan!',
+			];
+			return $res;
+		}
+
+	}
+	public function update_wallet($id){
+		$this->db->where('walletId', $id);
+		$result = $this->db->get('wallet');
+		if($result->num_rows() < 1){
+			$res = [
+				'status' => false,
+				'message' => 'Data tidak ditemukan',
+			];
+			return $res;
+		}
+		$userId = $this->input->post('userId');
+		$walletName = $this->input->post('walletName');
+		$walletColor = $this->input->post('walletColor');
+		$data = [
+			'walletName' => $walletName,
+			'walletColor' => $walletColor,
+			'walletUpdatedAt' => date('Y-m-d H:i:s'),
+		];
+		$this->db->where('walletId', $id);
+		if($this->db->update('wallet', $data)){
+			$res = [
+				'status' => true,
+				'message' => 'Wallet berhasil diupdate!',
+			];
+			return $res;
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Mohon maaf, terjadi kesalahan!',
+			];
+			return $res;
+		}
+	}
+	public function add_member_wallet($id){
+		$userId = $this->input->post('userId');
+		$memberId = $this->input->post('memberId');
+
+		$this->db->where('owWalletId', $id);
+		$this->db->where('owUserId', $userId);
+		$this->db->where('owIsAdmin', true);
+		$result = $this->db->get('our_wallet')->num_rows();
+		if($result < 1){
+			$res = [
+				'status' => false,
+				'message' => 'Anda tidak memiliki hak akses admin!',
+			];
+			return $res;
+		}
+
+		$data = [
+			"owWalletId" => $id,
+			"owUserId" => $memberId,
+			"owIsUserActive" => false,
+			"owIsAdmin" => false,
+			"owDate" => date('Y-m-d H:i:s'),
+		];
+		if($this->db->insert('our_wallet', $data)){
+			$res = [
+				'status' => true,
+				'message' => 'Member berhasil ditambahkan',
+			];
+			return $res;
+		}
+	}
+	public function get_user_to_wallet($id){
+		$list = '';
+		$no = 1;
+		$list_id = $this->get_member_of_wallet($id);
+		$length = count($list_id['data']);
+		foreach ($list_id['data'] as $l) {
+			if($no == $length){
+				$list.=$l['owUserId'];
+			}else{
+				$list.=$l['owUserId'].",";
+			}
+			$no++;
+		}
+		$query = "SELECT * FROM users WHERE userId NOT IN (".$list.")";
+		$result = $this->db->query($query);
+		if($result){
+			$data = $result->result_array();
+			$res = [
+				'status' => true,
+				'message' => 'Berhasil menampilkan data',
+				'data' => $data,
+			];
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Gagal akses data',
+			];
+		}
+		return $res;
+		
+	}
+	public function get_member_of_wallet($id){
+		$this->db->where('our_wallet.owIsUserActive',true);
+		$this->db->where('our_wallet.owWalletId',$id);
+		$this->db->join('users','users.userId = our_wallet.owUserId', 'left');
+		$result = $this->db->get('our_wallet');
+		if($result->num_rows() > 0){
+			$data = $result->result_array();
+			$res = [
+				'status' => true,
+				'message' => 'Berhasil menampilkan data',
+				'data' => $data,
+			];
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Gagal akses data',
+			];
+		}
+		return $res;
+	}
+	public function confirm_member_wallet($id){
+		$confirm = $this->input->post('confirm');
+		if($confirm == "Yes"){ 
+			$data['owIsUserActive']=true;
+			$this->db->where('owId',$id);
+			if($this->db->update('our_wallet', $data)){
+				$res = [
+					'status' => true,
+					'message' => 'Selamat! Anda telah tergabung',
+				];
+			}else{
+				$res = [
+					'status' => false,
+					'message' => 'Mohon maaf, telah terjadi kesalahan',
+				];
+			}
+			return $res;
+		}else{
+			$this->db->where('owId',$id);
+			if($this->db->delete('our_wallet')){
+				$res = [
+					'status' => true,
+					'message' => 'Terima kasih atas konfirmasinya',
+				];
+			}else{
+				$res = [
+					'status' => false,
+					'message' => 'Mohon maaf, telah terjadi kesalahan',
+				];
+			}
+			return $res;
+		}
+	}
+	public function invited_check_wallet($id){
+		$this->db->where('our_wallet.owIsUserActive', false);
+		$this->db->where('our_wallet.owUserId', $id);
+		$this->db->join('wallet','wallet.walletId = our_wallet.owWalletId', 'left');
+		$result = $this->db->get('our_wallet');
+		if($result){
+			$data = $result->result_array();
+			$res = [
+				'status' => true,
+				'message' => 'Berhasil menampilkan data',
+				'data' => $data,
+			];
+		}else{
+			$res = [
+				'status' => false,
+				'message' => 'Gagal akses data',
+			];
+		}
+		return $res;
+	}
+}
