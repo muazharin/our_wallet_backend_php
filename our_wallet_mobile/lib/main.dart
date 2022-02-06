3e0c17633437c81a9045b6b37546ecccdde763a7
Muazharin Alfan <alfanmuazharin@gmail.com>
2022-02-06 13:15:03 +0700

    tambah inisialisasi firebase

--- our_wallet_mobile/lib/main.dart
+++ our_wallet_mobile/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:our_wallet_mobile/pages/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Our Wallet',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
