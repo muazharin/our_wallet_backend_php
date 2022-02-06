b47816153d0a7a0c6bd1681941eec9cfade7a1ae
Muazharin Alfan <alfanmuazharin@gmail.com>
2022-02-06 13:20:18 +0700

    modifikasi tema

--- our_wallet_mobile/lib/theme.dart
+++ our_wallet_mobile/lib/theme.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color(0xffF50057);
+// Primary Color
+Color primaryWater = Color(0xff00b2ed);
+Color primaryWaterLight = Color(0xffe2f8ff);
+Color primaryWaterDark = Color(0xff2d7890);
+Color primaryBlood = Color(0xffe61445);
+Color primaryBloodLight = Color(0xffffeaee);
+Color primaryBloodDark = Color(0xff8a0c29);
+// Grayscale Color
+Color grayscaleCharcoal = Color(0xff181127);
+Color grayscaleCharcoalLight = Color(0xff393349);
+Color grayscaleCharcoalLightest = Color(0xff9093a6);
+Color grayscaleStone = Color(0xffd4d2d8);
+Color grayscaleStoneLight = Color(0xfff0f2f9);
+Color grayscaleStoneLightest = Color(0xfffbfbff);
+// Variant Color
+Color variantCactus = Color(0xff06D6A0);
+Color variantCactusLight = Color(0xffE0FAF3);
+Color variantCactusDark = Color(0xff038060);
+Color variantSunflower = Color(0xffF8C23A);
+Color variantSunflowerLight = Color(0xffFFF6DF);
+Color variantSunflowerDark = Color(0xffC69B2E);
